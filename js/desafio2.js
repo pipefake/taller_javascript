1b58281dfefce1a0c4393dfa6a2d695f5da493fd
pipefake <jfjimenezsalazar@gmail.com>
2021-09-07 17:36:37 -0500

    text fix

--- js/desafio2.js
+++ js/desafio2.js
@@ -2,5 +2,5 @@ function mostrar_imc() {
     var peso = document.getElementById("peso").value;
     var estatura = document.getElementById("estatura").value;
     var imc = (peso / (estatura * estatura)).toFixed(2);
-    document.getElementById("imc_show").innerHTML = "El total a pagar es: " + imc;
+    document.getElementById("imc_show").innerHTML = "Su IMC es : " + imc;
 }
--- js/desafio3.js
+++ js/desafio3.js
@@ -1,8 +1,10 @@
 function calcular_descuento() {
     var num_barras_no_dia = document.getElementById("barrasnodia").value;
-    var precio = num_barras_no_dia * 450;
+    var precioxbarra = 450;
+    var precio = num_barras_no_dia * precioxbarra;
     var descuento = precio * 0.5;
     var netoapagar = precio - descuento;
+    document.getElementById("precioxbarra_show").innerHTML = "El precio por barra es: " + precioxbarra;
     document.getElementById("precio_show").innerHTML = "El precio es: " + precio;
     document.getElementById("descuento_show").innerHTML = "El descuento es: " + descuento;
     document.getElementById("netoapagar_show").innerHTML = "El neto a pagar es: " + netoapagar;
